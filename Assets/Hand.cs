d44c59abd51a67914e72e95cd2adf80dceb28033
JamesD <jcd4912@gmail.com>
2018-02-25 05:56:24 -0600

    working send to socket

--- Assets/Hand.cs
+++ Assets/Hand.cs
@@ -3,75 +3,130 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hand : MonoBehaviour {
-    public enum State //possible states of our hand
-    {
-        EMPTY,
-        TOUCHING,
-        HOLDING
-    };
+	public enum State
+	{
+		EMPTY,
+		TOUCHING,
+		HOLDING
+	};
 
-    public OVRInput.Controller Controller = OVRInput.Controller.LTouch; //signifies which hand the controller represents
-    public State mHandState = State.EMPTY; //check to find out the state
-    public Rigidbody AttachPoint = null; //point to  determine where on the object we attatch our hand to when we grab it
-    public bool IgnoreContactPoint = false; 
-    private Rigidbody mHeldObject;
-    private FixedJoint mTempJoint;
-    private Vector3 mOldVelocity;
+	public Vector2 startPosition;
+	public Vector2 currentPosition = new Vector2();
+	float delay = 500F;
 
-    // Use this for initialization
-    void Start () {
-		if (AttachPoint == null)
-        {
-            AttachPoint = GetComponent<Rigidbody>();
-        }
+	WebSocket w = new WebSocket(new System.Uri("ws://localhost:8080"));
+
+	public OVRInput.Controller Controller = OVRInput.Controller.LTouch;
+	public State mHandState = State.EMPTY;
+	public Rigidbody AttachPoint = null;
+	public bool IgnoreContactPoint = false;
+	private Rigidbody mHeldObject;
+	private FixedJoint mTempJoint;
+	private Vector3 mOldVelocity;
+
+	IEnumerator startSocket() {
+		yield return StartCoroutine(w.Connect());
 	}
-	
-	void Update () { //call once per frame to check hand state and update things like the joint/velocity
-        switch (mHandState)
-        {
-            case State.TOUCHING:
-                if (mTempJoint == null && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, Controller) >= 0.5f)
-                {
-                    mHeldObject.velocity = Vector3.zero;
-                    mTempJoint = mHeldObject.gameObject.AddComponent<FixedJoint>();
-                    mTempJoint.connectedBody = AttachPoint;
-                    mHandState = State.HOLDING;
-                }
-                break;
-            case State.HOLDING:
-                if (mTempJoint != null && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, Controller) < 0.5f)
-                {
-                    Object.DestroyImmediate(mTempJoint);
-                    mTempJoint = null;
-                    mHandState = State.EMPTY;
-                }
-                mOldVelocity = OVRInput.GetLocalControllerAngularVelocity(Controller);
-                break;
-        }
-    }
 
-    void OnTriggerEnter(Collider collider) //checks if we are already holding something and if we can hold the item we are attempting to grab.
-    {
-        if (mHandState == State.EMPTY && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, Controller) < 0.5f) //if valid, set it  to be held and touching
-        {
-            GameObject temp = collider.gameObject;
-            if (temp != null && temp.layer == LayerMask.NameToLayer("grabbable") && temp.GetComponent<Rigidbody>() != null)
-            {
-                mHeldObject = temp.GetComponent<Rigidbody>();
-                mHandState = State.TOUCHING;
-            }
-        }
-    }
+	// Use this for initialization
+	void Start () {
+		if (AttachPoint == null)
+		{
+			AttachPoint = GetComponent<Rigidbody>();
+		}
+		StartCoroutine(startSocket ());
+	}
 
-    void OnTriggerExit(Collider collider) //if holding something, drop and set hand state to empty
-    {
-        if (mHandState != State.HOLDING)
-        {
-            if (collider.gameObject.layer == LayerMask.NameToLayer("grabbable"))
-            {
-                mHeldObject = null;
-                mHandState = State.EMPTY;
-            }
-        }
-    }
+	// Update is called once per frame
+	void Update () {
+		switch (mHandState)
+		{
+		case State.TOUCHING:
+			if (mTempJoint == null && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, Controller) >= 0.5f)
+			{
+				mHeldObject.velocity = Vector3.zero;
+				mTempJoint = mHeldObject.gameObject.AddComponent<FixedJoint>();
+				mTempJoint.connectedBody = AttachPoint;
+				mHandState = State.HOLDING;
+			}
+			break;
+		case State.HOLDING:
+			if (mTempJoint != null && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, Controller) < 0.5f)
+			{
+				Object.DestroyImmediate(mTempJoint);
+				mTempJoint = null;
+				throwObject();
+				mHandState = State.EMPTY;
+			}
+			mOldVelocity = OVRInput.GetLocalControllerAngularVelocity(Controller);
+			break;
+		}
+
+		updateCurrentPosition();
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (mHandState == State.EMPTY && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, Controller) < 0.5f)
+		{
+			GameObject temp = collider.gameObject;
+			if (temp != null && temp.layer == LayerMask.NameToLayer("grabbable") && temp.GetComponent<Rigidbody>() != null)
+			{
+				mHeldObject = temp.GetComponent<Rigidbody>();
+				mHandState = State.TOUCHING;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+		if (mHandState != State.HOLDING)
+		{
+			if (collider.gameObject.layer == LayerMask.NameToLayer("grabbable"))
+			{
+				mHeldObject = null;
+				mHandState = State.EMPTY;
+			}
+		}
+	}
+
+	Vector2 getStartPosition() {
+		Vector3 pos = OVRInput.GetLocalControllerPosition (OVRInput.Controller.RTouch);
+		return new Vector2 (pos.y, pos.z);
+	}
+
+	void updateCurrentPosition() {
+
+		if (startPosition == null) {
+			startPosition = getStartPosition ();
+			return;
+		}
+
+		Vector3 pos = OVRInput.GetLocalControllerPosition (OVRInput.Controller.RTouch);
+
+		Debug.Log (startPosition);
+
+
+		// Send across socket connection
+		currentPosition.x = pos.z - startPosition.x;
+		currentPosition.y = pos.y - startPosition.y;
+
+		if (delay < 0) {
+			w.SendString(currentPosition.x + "," + currentPosition.y);
+			delay = 500F;
+		}
+
+		delay -= Time.deltaTime * 1000;
+		Debug.Log (currentPosition);
+	}
+
+	private void throwObject()
+	{
+		mHeldObject.velocity = OVRInput.GetLocalControllerVelocity(Controller);
+		if (mOldVelocity != null)
+		{
+			mHeldObject.angularVelocity = OVRInput.GetLocalControllerAngularVelocity(Controller);
+		}
+		mHeldObject.maxAngularVelocity = mHeldObject.angularVelocity.magnitude;
+	}
 }
